6a9c2ffdc1881ca30834bd1c5f27db668e88468c
dimabe-odoo <desarrolloti@dimabe.cl>
2020-08-24 19:43:42 -0400

    fix error

--- r3m/models/stock_quant.py
+++ r3m/models/stock_quant.py
@@ -69,23 +69,23 @@ class StockQuant(models.Model):
                 lot = self.env['stock.production.lot'].search([('id', '=', q.lot_id.id)])
                 for picking_id in pickings:
                     if q.lot_id.id in picking_id.move_line_ids_without_package.mapped('lot_id').mapped('id'):
-                    q.write({
-                        'picking_id': picking_id.id,
-                        'r3m_partner_id': picking_id.partner_id.id,
-                        'r3m_eta': picking_id.r3m_eta,
-                        'r3m_po': picking_id.origin,
-                        'r3m_bl': picking_id.r3m_bl,
-                        'r3m_container': picking_id.r3m_container,
-                        'r3m_booking': picking_id.r3m_booking,
-                        'r3m_rol_weight': lot.kilos,
-                        'r3m_gramaje': self.variant_search(q.product_id.id, 'gramaje'),
-                        'r3m_paper_type': self.variant_search(q.product_id.id, 'tipo de papel'),
-                        'r3m_format': self.variant_search(q.product_id.id, 'FORMATO DE BOBINA'),
-                        'r3m_linear_m': lot.linear_m,
-                        'r3m_order': picking_id.r3m_order,
-                        'r3m_oc': picking_id.r3m_po,
-                        'r3m_picking_date': picking_id.create_date,
-                    })
+                        q.write({
+                            'picking_id': picking_id.id,
+                            'r3m_partner_id': picking_id.partner_id.id,
+                            'r3m_eta': picking_id.r3m_eta,
+                            'r3m_po': picking_id.origin,
+                            'r3m_bl': picking_id.r3m_bl,
+                            'r3m_container': picking_id.r3m_container,
+                            'r3m_booking': picking_id.r3m_booking,
+                            'r3m_rol_weight': lot.kilos,
+                            'r3m_gramaje': self.variant_search(q.product_id.id, 'gramaje'),
+                            'r3m_paper_type': self.variant_search(q.product_id.id, 'tipo de papel'),
+                            'r3m_format': self.variant_search(q.product_id.id, 'FORMATO DE BOBINA'),
+                            'r3m_linear_m': lot.linear_m,
+                            'r3m_order': picking_id.r3m_order,
+                            'r3m_oc': picking_id.r3m_po,
+                            'r3m_picking_date': picking_id.create_date,
+                        })
             except Exception:
                 raise models.ValidationError(Exception)
 
